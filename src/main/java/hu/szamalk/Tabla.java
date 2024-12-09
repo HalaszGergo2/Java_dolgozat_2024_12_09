f3eb9c689651d6b2785f0ea0523cd4234e331c69
HalaszGergo2 <xlomox12@gmail.com>
2024-12-09 14:11:05 +0100

    f1 4

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -6,12 +6,24 @@ public class Tabla {
 
     public Tabla(char üresCella) {
         T = new char[8][8];
-        ÜresCella = üresCella;
+        this.ÜresCella = üresCella;
         for (int i = 0; i < T.length; i++) {
             for(int j = 0; j < T.length; j++){
-                T[i][j] = ÜresCella;
+                T[i][j] = üresCella;
             }
         }
     }
+
+    public void Megjelenít(){
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                    System.out.print(T[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+
 
 }
